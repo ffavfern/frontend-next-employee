b235df19528e91bae7fd465d8038e1585c68ee04
ffavfrn <ffern27744@gmail.com>
2022-08-10 14:15:26 +0700

    4

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,21 @@ import Navbar from "../components/Navbar";
 
 export default function _document() {
   return (
-    <div>
-  {'{'}/* {/* CSS only */} */{'}'}
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
-  {'{'}/* {/* JavaScript Bundle with Popper */} */{'}'}
-  <navbar>
-    <main>
-      <nextscript>
-      </nextscript></main></navbar></div>
-
+    <Html>
+      
+      <Head>
+        {"{"}/* {/* CSS only */} */{"}"}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+          crossOrigin="anonymous"
+        />
+        {"{"}/* {/* JavaScript Bundle with Popper */} */{"}"}
+      </Head>
+      <Navbar />
+      <Main />
+      <NextScript />
+    </Html>
   );
 }
