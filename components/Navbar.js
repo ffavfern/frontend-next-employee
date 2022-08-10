55ebe703e8d7973110fd0989d5bd51dd61e79360
ffavfrn <ffern27744@gmail.com>
2022-08-10 14:07:05 +0700

    2

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,39 +5,40 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
+    <nav classname="navbar navbar-expand-lg bg-light">
+  <div classname="container-fluid">
+    <a classname="navbar-brand" href="#">
+      Navbar
+    </a>
+    <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span classname="navbar-toggler-icon">
+      </span></button>
+    <div classname="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul classname="navbar-nav me-auto mb-2 mb-lg-0">
+        <li classname="nav-item">
+          <a classname="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form classname="d-flex" role="search">
+        <input classname="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button classname="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
     </div>
-  </nav>
+  </div>
+</nav>
+
   </>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,27 +4,14 @@ import Navbar from "../components/Navbar";
 
 export default function _document() {
   return (
-    <Html>
-      <Head>
-        {/* <!-- CSS only --> */}
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-          crossorigin="anonymous"
-        />
-        {/* <!-- JavaScript Bundle with Popper --> */}
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
-          crossorigin="anonymous"
-        ></script>
-      </Head>
-      <body>
-        <Navbar />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <div>
+  {'{'}/* {/* CSS only */} */{'}'}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
+  {'{'}/* {/* JavaScript Bundle with Popper */} */{'}'}
+  <navbar>
+    <main>
+      <nextscript>
+      </nextscript></main></navbar></div>
+
   );
 }
